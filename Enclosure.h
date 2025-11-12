2378e49d45ed70eb0de5328736e775211f679995
Hamzawiiz24 <hamzawiigaming24@gmail.com>
2025-11-12 15:17:51 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -3,7 +3,23 @@
 
 #include "Animal.h"
 
-//define enclosure class here 
+  
+class Enclosure() {
+
+private:
+	int capacity; 
+	int currentCount;
+	Enclosure* Animal;
+
+public:
+	enclosure();
+	enclosure(int capacity, int currentCount, Enclosure * Animal);
+	void addAnimal(Animal * a);
+	void displayAnimal();
+	~enclosure();
+
+}
+
 
 
 
